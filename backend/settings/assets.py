50051fcdc1289dd8667539bc4aeefd5bf388a800
Afonasev Evgeniy <ea.afonasev@gmail.com>
2017-09-25 19:39:35 +0500

    update docker-compose config for developers

--- backend/settings/assets.py
+++ backend/settings/assets.py
@@ -1,15 +1,11 @@
 import os
 
 from .env import PRODUCTION
-from .paths import BASE_DIR, PROJECT_DIR
+from .paths import BASE_DIR
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_DIR, "static"),
-)
 
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
